f84b7b65498a22133ae3068b7a09195093ba979c
Eric Schrijver <eric@ericschrijver.nl>
2014-07-13 22:24:19 +0200

    Simple meteor app to show, hide and delete the Raduga images
    - Login not implemented yet
    - Designed to use with an existing mongodb, not meteor’s one.
      ObjectID handling might be slightly different

---
+++ client/templates.js
@@ -0,0 +1,31 @@
+var str2ID = function(id) {
+    id = id.replace('ObjectID("','');
+    id = id.replace('")','');
+    id = new Meteor.Collection.ObjectID(id)
+    return id;
+}
+
+Template.photos.photos = function () {
+    var photos = Photos.find();
+    if (!photos) { return []; }
+    return photos;
+};
+
+Template.photos.events = {
+    'click button.delete': function(e) {
+        var id = str2ID(e.target.getAttribute("rel"));
+        console.log("trying to remove", id);
+        Photos.remove(id);
+    },
+    'click button.show': function(e) {
+        var id = str2ID(e.target.getAttribute("rel"));
+        console.log("trying to show", id);
+        Photos.update(id, {"$set": {"processed": true}});
+    },
+    'click button.hide': function(e) {
+        var id = str2ID(e.target.getAttribute("rel"));
+        console.log("trying to hide", id);
+        Photos.update(id, {"$set": {"processed": false}});
+    }
+};
+
